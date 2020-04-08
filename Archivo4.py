bb8f0b207b4a3560647049a06ce1589f2ef62b73
Alejandro <alejandro_e_gonzalez@yahoo.com.ar>
2020-04-08 10:52:15 -0300

    Cuarto commit rama Master - previo version2 - inicio.html archivo4.py

---
+++ Archivo4.py
@@ -0,0 +1,9 @@
+# Archivo4.py
+# Rama Master
+# Primera modificación
+# posterior a Versión 1 
+
+apellido = "González     "
+nombre = "Alejandro E. "
+apenom = apellido.strip() + " " + nombre
+print(apenom.upper())

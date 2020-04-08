2ec8b9bc2937cfc55daac7d0079f7a4fe5b5a1b8
Alejandro <alejandro_e_gonzalez@yahoo.com.ar>
2020-04-08 16:25:36 -0300

    commit de RamaProgramador para version3

--- Archivo3.py
+++ Archivo3.py
@@ -3,7 +3,12 @@
 # Segunda Modificación
 # Versión 1 Lista
 
+# Modificación para Version 3
+# Hecha en rama RamaProgramador
+
 apellido = "González     "
 nombre = "Alejandro Emilio "
 apenom = apellido.strip() + " " + nombre
 print(apenom.upper())
+
+# Otra modificacion hecha en rama RamaProgramador
